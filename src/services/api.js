23f1b78238bd0f4e82d1bcdb72bb7b60ee41c3e9
Krittin Jaruvisut <krittin.j58@rsu.ac.th>
2020-09-15 17:32:47 +0700

    added config api services

---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+const API_URL = 'http://www.omdbapi.com';
+const API_KEY = 'efcee23b';
+
+export default {
+  API_URL,
+  API_KEY,
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,37 @@
 <template>
-<h1>Home</h1>
+    <div>
+      <input type="text" v-model="movieId" v-on:keyup.enter="getMovie(movieId)">
+      <p>{{ movieResponse }}</p>
+    </div>
 </template>
 
 <script>
+import config from '../services/api';
+
 export default {
   name: 'Home',
 
   data() {
     return {
-      //
+      movieResponse: null,
+      movieId: null,
     };
+  },
+
+  mounted() {
+    this.getMovie();
+  },
+
+  methods: {
+    async getMovie(id) {
+      try {
+        const url = `${config.API_URL}/?apikey=${config.API_KEY}&i=${id}`;
+        const response = await fetch(url);
+        this.movieResponse = await response.json();
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 };
 </script>
